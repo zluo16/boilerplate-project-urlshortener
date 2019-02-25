520f8c1012273077531b1943f8572a776b3a7b10
Mike <zluo@alumni.berklee.edu>
2019-02-25 17:39:17 -0500

    Moved check and create short url logic out of the controller and into the Url Model/Schema

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Url = require('./models/url');
-const dns = require('dns');
 
 // your first API endpoint... 
 router.get("/hello", function (req, res) {
@@ -11,22 +10,7 @@ router.get("/hello", function (req, res) {
 // Post url
 router.post("/shorturl/new/", function(req, res) {
   let original_url = req.body.url;
-  Url.findOne()
-    .sort('-short_url')
-    .exec(function(err, url) {
-    if (err) res.json(err);
-    let short_url = url.short_url + 1;
-    let originalNoProtocol = original_url.split('//')[1];
-    dns.lookup(originalNoProtocol, function(err) {
-      if (err) res.json({ error: 'invalid URL' });
-      let newUrl = new Url({ original_url, short_url });
-      newUrl.save(function(err, url) {
-        if (err) res.json(err);
-        let short_url = url.short_url;
-        res.redirect(`/api/shorturl/get/${short_url}`);
-      });
-    });
-  });
+  Url.checkAndCreateNewShorturl(original_url, res);
 });
 
 // Redirect to shortcut url
--- models/url.js
+++ models/url.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
 
 const urlSchema = new mongoose.Schema({
   original_url: { type: String, required: true },
   short_url: { type: Number, unique: true, required: true }
 });
+
+urlSchema.statics.checkAndCreateNewShorturl = function(original_url, res) {
+  this.findOne()
+  .sort('-short_url')
+  .exec((err, url) => {
+    if (err) res.json(err);
+    let short_url = url.short_url + 1;
+    let originalNoProtocol = original_url.split('//')[1];
+    dns.lookup(originalNoProtocol, (err) => {
+      if (err) res.json({ error: 'invalid URL' });
+      let newUrl = new this({ original_url, short_url });
+      newUrl.save((err, url) => {
+        if (err) res.json(err);
+        let short_url = url.short_url;
+        res.redirect(`/api/shorturl/get/${short_url}`);
+      });
+    });
+  });
+};
 
 module.exports = mongoose.model('Url', urlSchema);
